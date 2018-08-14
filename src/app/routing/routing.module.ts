38faca9a5df8e581e4ab54726a4c3c287707467c
atodorovska <ana5todorovska@gmail.com>
2018-08-14 14:56:49 +0200

    small change

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from '../home/home.component';
 import {SearchComponent} from '../search/search.component';
-import {LoginComponent} from "../login/login.component";
+import {LoginComponent} from '../login/login.component';
 
 const routes: Routes = [
   {
